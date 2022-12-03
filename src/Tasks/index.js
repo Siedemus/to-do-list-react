4b84e5dbeeb19e25f2a3fd275c134b42fffec2bd
Bartosz Sakiewa <bar11sak7@gmail.com>
2022-12-03 17:50:21 +0100

    add tasks component

---
+++ src/Tasks/index.js
@@ -0,0 +1,19 @@
+import "./style.css";
+
+const Tasks = ({ tasks, hideDoneTasks }) => {
+    return (
+        <ul className="list">
+            {tasks.map(task => (
+                <li key={task.id} className={`list__item${task.done && hideDoneTasks ? " list__item--hidden" : ""}`}>
+                    <button className="list__button list__button--done">{task.done ? "✔" : ""}</button>
+                    <p className={`list__text${task.done ? " list__text--done" : ""}`}>{task.content}</p>
+                    <button class="list__button list__button-remove">🗑</button>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default Tasks;
+
+
