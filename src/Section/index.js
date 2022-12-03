cb459a1e9ba04dc7745dacbba7f5f94e4ba182a8
Bartosz Sakiewa <bar11sak7@gmail.com>
2022-12-03 17:50:07 +0100

    add section component

---
+++ src/Section/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+const Section = ({subHeader, form, container, tasksList}) => (
+    <section className="section">
+        {subHeader}
+        {form}
+        {container}
+        {tasksList}
+    </section>
+)
+
+export default Section;
