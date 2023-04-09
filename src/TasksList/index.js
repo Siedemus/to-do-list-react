d4153dbfbff2a2119070438663083f309dd2335c
Bartosz Sakiewa <bar11sak7@gmail.com>
2023-04-10 00:45:56 +0200

    make code more preety and read-able

--- src/TasksList/index.js
+++ src/TasksList/index.js
@@ -5,10 +5,21 @@ const TasksList = ({ tasks, hideDone, removeTask }) => (
         {tasks.map(task => (
             <li
                 key={task.id}
-                className={`list__item ${task.done && hideDone ? " list__item--hidden" : ""}`}>
-                <button className="list__button list__button--done">{task.done ? "✔" : ""}</button>
-                <p className={`list__text${task.done ? " list__text--done" : ""}`}>{task.content}</p>
-                <button onClick={() => removeTask(task.id)} className="list__button list__button-remove">🗑</button>
+                className={`list__item ${task.done && hideDone ? " list__item--hidden" : ""}`}
+            >
+                <button
+                    className="list__button list__button--done">
+                    {task.done ? "✔" : ""}
+                </button>
+                <p
+                    className={`list__text${task.done ? " list__text--done" : ""}`}>
+                    {task.content}
+                </p>
+                <button
+                    onClick={() => removeTask(task.id)}
+                    className="list__button list__button-remove">
+                    🗑
+                </button>
             </li>
         ))}
     </ul >
