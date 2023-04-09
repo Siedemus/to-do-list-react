32ad4b791683da9f429ad053286c7f3d87d4ef57
Bartosz Sakiewa <bar11sak7@gmail.com>
2023-04-09 19:20:24 +0200

    change class to className

--- src/TasksList/index.js
+++ src/TasksList/index.js
@@ -8,7 +8,7 @@ const TasksList = ({ tasks, hideDone }) => (
                 className={`list__item ${task.done && hideDone ? " list__item--hidden" : ""}`}>
                 <button className="list__button list__button--done">{task.done ? "✔" : ""}</button>
                 <p className={`list__text${task.done ? " list__text--done" : ""}`}>{task.content}</p>
-                <button class="list__button list__button-remove">🗑</button>
+                <button className="list__button list__button-remove">🗑</button>
             </li>
         ))}
     </ul >
