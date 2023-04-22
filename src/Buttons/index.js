98eaf8c116eede951b4d7bd231b8b6651838119b
Bartosz Sakiewa <bar11sak7@gmail.com>
2023-04-22 22:59:57 +0200

    add ButtonsContainer and Button styled components

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,23 +1,18 @@
-import "./style.css";
+import { ButtonsContainer, Button } from "./styled";
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => {
-    return tasks.length > 0 && (
-        <div className="container__buttons">
-            <button
-                onClick={toggleHideDone}
-                className="container__button"
-            >
-                {hideDone ? "Pokaż" : "Ukryj"} ukończone
-            </button>
-            <button
-                className="container__button"
-                disabled={tasks.every(({ done }) => done)}
-                onClick={setAllDone}
-            >
-                Ukończ wszystkie
-            </button>
-        </div>
+  return (
+    tasks.length > 0 && (
+      <ButtonsContainer>
+        <Button onClick={toggleHideDone}>
+          {hideDone ? "Pokaż" : "Ukryj"} ukończone
+        </Button>
+        <Button disabled={tasks.every(({ done }) => done)} onClick={setAllDone}>
+          Ukończ wszystkie
+        </Button>
+      </ButtonsContainer>
     )
-}
+  );
+};
 
-export default Buttons
+export default Buttons;
---
+++ src/Buttons/styled.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const ButtonsContainer = styled.div`
+  align-self: center;
+
+  .container__buttons {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+`;
+
+const Button = styled.button`
+  border: none;
+  background-color: transparent;
+  padding: 15px;
+  color: teal;
+  font-size: 18px;
+  transition: 0.5s;
+
+  &:hover {
+    color: rgb(0, 177, 177);
+  }
+
+  &:active {
+    color: rgb(0, 78, 78);
+  }
+
+  &:disabled {
+    color: rgba(125, 125, 125, 0.389);
+  }
+`;
+
+export { ButtonsContainer, Button };
