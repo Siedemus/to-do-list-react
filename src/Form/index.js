84d57607d5b611bbe2ade3b46cbf5e85597a79e4
Bartosz Sakiewa <bar11sak7@gmail.com>
2023-04-22 22:51:32 +0200

    add Button styled component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,31 +1,31 @@
 import { useState } from "react";
-import "./style.css";
+import { StyledForm, Input, Button } from "./styled";
 
 const Form = ({ addNewTask }) => {
-    const [newContent, setNewContent] = useState("")
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        const trimmedNewTaskContent = newContent.trim()
-        if (!trimmedNewTaskContent) {
-            return setNewContent("");
-        };
+  const [newContent, setNewContent] = useState("");
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    const trimmedNewTaskContent = newContent.trim();
+    if (!trimmedNewTaskContent) {
+      return setNewContent("");
+    }
 
-        addNewTask(trimmedNewTaskContent)
-        setNewContent("")
-    };
+    addNewTask(trimmedNewTaskContent);
+    setNewContent("");
+  };
 
-    return (
-        <>
-            <form onSubmit={onFormSubmit} className="form">
-                <input
-                    onChange={({ target }) => setNewContent(target.value)}
-                    value={newContent}
-                    className="form__input"
-                    placeholder="Co jest do zrobienia?" />
-                <button className="form__button">Dodaj zadanie</button>
-            </form>
-        </>
-    )
-}
+  return (
+    <>
+      <StyledForm onSubmit={onFormSubmit}>
+        <Input
+          onChange={({ target }) => setNewContent(target.value)}
+          value={newContent}
+          placeholder="Co jest do zrobienia?"
+        />
+        <Button className="form__button">Dodaj zadanie</Button>
+      </StyledForm>
+    </>
+  );
+};
 
 export default Form;
---
+++ src/Form/styled.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+  background-color: white;
+  padding: 25px;
+  display: flex;
+  gap: 15px;
+
+  @media (max-width: 767px) {
+    .form {
+      flex-direction: column;
+    }
+  }
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  border: 1px solid rgb(214, 214, 214);
+  flex-grow: 1;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  color: white;
+  background-color: teal;
+  border: none;
+  transition: background 1s, 1s;
+
+  &:hover {
+    background-color: rgb(0, 181, 181);
+    transform: scale(1.05);
+  }
+
+  &:active {
+    outline: 2px black solid;
+  }
+`;
+
+export { StyledForm, Input, Button };
