c3bd949be81955175c578d4548192772c9b124dc
Bartosz Sakiewa <barsak1177@gmail.com>
2023-05-21 19:57:15 +0200

    apply store provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { GlobalStyles } from "./globalStyles";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "styled-components";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const theme = {
   colors: {
@@ -19,10 +21,12 @@ const theme = {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
