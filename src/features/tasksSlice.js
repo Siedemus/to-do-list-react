5f3f1c96db8664e9d5b6f27f2504b7c1c9e607fd
Bartosz Sakiewa <barsak1177@gmail.com>
2023-05-21 20:39:38 +0200

    add toggleHideDone

--- src/features/tasksSlice.js
+++ src/features/tasksSlice.js
@@ -1,18 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const tasksSlice = createSlice({
-    name: 'tasks',
-    initialState: {
-        tasks: [
-        ],
+  name: "tasks",
+  initialState: {
+    tasks: [{content: "hello", id: 1, done:true}],
+    hideDone: false,
+  },
+  reducers: {
+    addTask: ({ tasks }, { payload }) => {
+      tasks.push(payload);
     },
-    reducers: {
-        addTask: ({tasks}, {payload}) => {
-            tasks.push(payload);
-        },
+    toggleHideDone: (tasks) => {
+      tasks.hideDone = !tasks.hideDone;
     },
+  },
 });
 
-export const { addTask } = tasksSlice.actions;
-export const selectTasks = state => state.tasks;
-export default tasksSlice.reducer
+export const { addTask, toggleHideDone } = tasksSlice.actions;
+export const selectTasks = (state) => state.tasks;
+export default tasksSlice.reducer;
